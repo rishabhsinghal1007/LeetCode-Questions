81830bcfa478f99dd4339fbbeb5a4fe126bb9723
rishabhsinghal1007 <rishabhsinghal1007@gmail.com>
2021-06-01 00:39:48 +0530

    commit

---
+++ LinkedList/Q19_removeNthNodeFromEndLL.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null)
+            return head;
+
+        ListNode slow = head, fast = head;
+
+        while (n-- > 0) {
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            ListNode rNode = slow;
+            head = rNode.next;
+            rNode.next = null;
+            return head;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        ListNode rNode = slow.next;
+        slow.next = rNode.next;
+        rNode.next = null;
+
+        return head;
+    }
+}
